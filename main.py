4ba60d5d2837af558dcd1e201d7b293a92cf727c
ssebastianoo <sebastianogirotto05@gmail.com>
2020-10-05 20:52:09 +0200

    intents

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
 os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True" 
 os.environ["JISHAKU_HIDE"] = "True"
 
-bot = commands.AutoShardedBot(command_prefix = commands.when_mentioned_or("//"), case_insensitive = True, description = "Bot ufficiale del server Mappa Discord Italia")
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.AutoShardedBot(intents = intents, command_prefix = commands.when_mentioned_or("//"), case_insensitive = True, description = "Bot ufficiale del server Mappa Discord Italia")
 bot.load_extension("jishaku")
 
 @bot.event
